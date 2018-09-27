2f10795a7ee69b973db7f4ac71908fe093c37e49
Jerome Renaux <jerome.renaux@gmail.com>
2018-09-27 23:14:45 +0200

    Adding visits counter

--- js/game.js
+++ js/game.js
@@ -15,6 +15,10 @@ Game.create = function(){
     this.scoreTxt = this.add.bitmapText(txt.x+txt.width+10, 0, 'font', '0', 38).setOrigin(0);
     Game.setScore();
 
+    var txt = this.add.bitmapText(0, 600, 'font', 'VISITS:', 20).setOrigin(0,1);
+    this.visitsTxt = this.add.bitmapText(txt.x+txt.width+10, 600, 'font', '0', 20).setOrigin(0,1);
+    Game.setVisits();
+
     var logo = this.add.image(400,300,'logo').setInteractive();
     logo.on('pointerdown',function(){
         Game.updateScore(10);
@@ -35,6 +39,15 @@ Game.setScore = function(){
     // Don't fortget to parseInt(), all values are stored as strings in localStorage
     Game.scene.score = parseInt(localStorage.getItem('score')) || 0;
     Game.scene.scoreTxt.setText(Game.scene.score);
+};
+
+Game.setVisits = function(){
+    // Gets the value stored in localStorage, or 0 if nothing is found
+    // Don't fortget to parseInt(), all values are stored as strings in localStorage
+    Game.scene.visits = (parseInt(localStorage.getItem('visits')) || 0) + 1;
+    Game.scene.visitsTxt.setText(Game.scene.visits);
+    // Immediately increment the stored visits counter
+    localStorage.setItem('visits',Game.scene.visits);
 };
 
 Game.updateScore = function(increment){
